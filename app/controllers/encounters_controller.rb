bc8431b4d6db1438d00e51e68296f0c88c402af5
Yamiko <yamikomsosa@yahoo.co.uk>
2012-02-02 15:54:29 +0200

    Updated encounters controller diagnoses method to return an empty first option to remove auto selection in view

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -599,7 +599,8 @@ class EncountersController < ApplicationController
 
 		@suggested_answers = valid_answers.sort.uniq.reject{|answer| filter_list.include?(answer) }.uniq[0..10]
 		@suggested_answers = @suggested_answers - params[:search_filter].split(',') rescue @suggested_answers
-		render :text => "<li>" + @suggested_answers.join("</li><li>") + "</li>"
+		@guggested_answers = [""] + @suggested_answers
+		render :text => "<li></li>" + "<li>" + @suggested_answers.join("</li><li>") + "</li>"
 	end
 
   def daignosis_details
