f5ee41cac9c9c055d0237316ac868716855c4e8c
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2012-08-01 18:20:16 +0200

    find patient using barcode

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -18,7 +18,7 @@ class PeopleController < GenericPeopleController
     @task = main_next_task(Location.current_location, @person.patient, session_date.to_date)
     @arv_number = PatientService.get_patient_identifier(@person, 'ARV Number')
 	  @patient_bean = PatientService.get_patient(@person)
-    render :layout => 'menu'
+    render :layout => false
   end
  
   def create
