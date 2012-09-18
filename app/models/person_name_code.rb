258b2b3f15eb49ad34c3a1e6815147578f36fed9
Blessings <blessings.mhango@gmail.com>
2012-09-18 13:55:23 +0200

    modified patient search methods

--- app/models/person_name_code.rb
+++ app/models/person_name_code.rb
@@ -2,9 +2,9 @@ class PersonNameCode < ActiveRecord::Base
   set_table_name "person_name_code"
   set_primary_key "person_name_code_id"
   include Openmrs
-  
+
   belongs_to :person_name, :conditions => {:voided => 0}
-  
+
   def self.rebuild_person_name_codes
     PersonNameCode.delete_all
     names = PersonName.find(:all)
@@ -20,7 +20,7 @@ class PersonNameCode < ActiveRecord::Base
     }
   end
 
-  # Find all of the matches for this code  
+  # Find all of the matches for this code
   # When we order them we want to prioritize:
   #   - Items where the search text matches the actual name ("Mary" versus "Mari")
   #   - Items where the actual soundex starts with the soundex for the search text ("M4" versus "M6")
@@ -29,6 +29,7 @@ class PersonNameCode < ActiveRecord::Base
   #       LENGTH(#{field_name}_code) - LENGTH(?) ASC,
   #   - Popularity of the search text
   #   - Search text alphabetized
+=begin
   def self.find_most_common(field_name, search_string)
     soundex = (search_string || '').soundex
     self.find_by_sql([
@@ -46,5 +47,34 @@ class PersonNameCode < ActiveRecord::Base
          #{field_name} ASC \
        LIMIT 10",
        "#{soundex}%", search_string, search_string, soundex, soundex, soundex])
+  end
+=end
+
+
+  def self.find_most_common(field_name, search_string)
+    soundex = (search_string || '').soundex
+    self.find_by_sql([
+      "SELECT DISTINCT #{field_name} AS #{field_name}, person_name.person_name_id AS id
+       FROM person_name_code \
+       INNER JOIN person_name ON person_name_code.person_name_id = person_name.person_name_id \
+       WHERE  person_name.voided = 0 AND #{field_name}_code LIKE ? \
+       GROUP BY #{field_name} \
+       ORDER BY \
+        CASE INSTR(#{field_name},?) WHEN 0 THEN 9999 ELSE INSTR(#{field_name},?) END ASC \
+       LIMIT 10",
+       "#{soundex}%", search_string, search_string])
+  end
+
+  def self.find_top_ten(field_name)
+    self.find_by_sql([
+      "SELECT DISTINCT #{field_name} AS #{field_name}, person_name.person_name_id AS id
+       FROM person_name_code \
+       INNER JOIN person_name ON person_name_code.person_name_id = person_name.person_name_id \
+       INNER JOIN person ON person.person_id = person_name.person_id \
+       WHERE person.voided = 0 AND person_name.voided = 0 \
+       GROUP BY #{field_name} \
+       ORDER BY \
+         #{field_name} ASC \
+       LIMIT 10"])
   end
 end
