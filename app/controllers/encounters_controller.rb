5c4d50fa2d62640aa011cc08544048d2c085ef2b
phundi <kennethkapundi1@gmail.com>
2012-10-11 14:57:07 +0200

    updates to diagnoses requiring details and specification

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -105,7 +105,17 @@ class EncountersController < GenericEncountersController
 				Location.current_health_center.id, Program.find_by_name('TB PROGRAM').id, 
 				Program.find_by_name('MDR-TB PROGRAM').id
 			]).last.closed? rescue true
-=end        
+=end
+    if  ['INPATIENT_DIAGNOSIS', 'ADMISSION_DIAGNOSIS', 'DISCHARGE_DIAGNOSIS', 'REFERRAL_DIAGNOSIS'].include?((params[:encounter_type].upcase rescue ''))
+			diagnosis_concept_set_id = ConceptName.find_by_name("Diagnoses requiring specification").concept.id
+			diagnosis_concepts = Concept.find(:all, :joins => :concept_sets, :conditions => ['concept_set = ?', diagnosis_concept_set_id])
+			@diagnoses_requiring_specification = diagnosis_concepts.map{|concept| concept.fullname.upcase}.join(';')
+
+			diagnosis_concept_set_id = ConceptName.find_by_name("Diagnoses requiring details").concept.id
+			diagnosis_concepts = Concept.find(:all, :joins => :concept_sets, :conditions => ['concept_set = ?', diagnosis_concept_set_id])
+			@diagnoses_requiring_details = diagnosis_concepts.map{|concept| concept.fullname.upcase}.join(';')
+    end
+
 		if (params[:encounter_type].upcase rescue '') == 'PRESENTING_COMPLAINTS'
 			complaint_concept_set_id = ConceptName.find_by_name("Presenting complaints requiring specification").concept.id
 			complaint_concepts = Concept.find(:all, :joins => :concept_sets, :conditions => ['concept_set = ?', complaint_concept_set_id])
@@ -393,7 +403,7 @@ class EncountersController < GenericEncountersController
 				['Vasectomy', 'VASECTOMY']
 			],
 			'male_family_planning_methods' => [
-			['',''],
+        ['',''],
 				['Male condoms', 'MALE CONDOMS'],
 				['Withdrawal', 'WITHDRAWAL'],
 				['Rhythm method', 'RYTHM METHOD'],
@@ -402,7 +412,7 @@ class EncountersController < GenericEncountersController
 				['Other','OTHER']
 			],
 			'female_family_planning_methods' => [
-			['',''],
+        ['',''],
 				['Oral contraceptive pills', 'ORAL CONTRACEPTIVE PILLS'],
 				['Depo-Provera', 'DEPO-PROVERA'],
 				['IUD-Intrauterine device/loop', 'INTRAUTERINE CONTRACEPTION'],
@@ -416,7 +426,7 @@ class EncountersController < GenericEncountersController
 				['Other','OTHER']
 			],
 			'drug_list' => [
-			['',''],
+        ['',''],
 				["Rifampicin Isoniazid Pyrazinamide and Ethambutol", "RHEZ (RIF, INH, Ethambutol and Pyrazinamide tab)"],
 				["Rifampicin Isoniazid and Ethambutol", "RHE (Rifampicin Isoniazid and Ethambutol -1-1-mg t"],
 				["Rifampicin and Isoniazid", "RH (Rifampin and Isoniazid tablet)"],
@@ -480,10 +490,10 @@ class EncountersController < GenericEncountersController
 			],
 			'lab_orders' => {
 				"Blood" => ["Full blood count", "Malaria parasite", "Group & cross match", "Urea & Electrolytes", "CD4 count", "Resistance",
-				"Viral Load", "Cryptococcal Antigen", "Lactate", "Fasting blood sugar", "Random blood sugar", "Sugar profile",
-				"Liver function test", "Hepatitis test", "Sickling test", "ESR", "Culture & sensitivity", "Widal test", "ELISA",
-				"ASO titre", "Rheumatoid factor", "Cholesterol", "Triglycerides", "Calcium", "Creatinine", "VDRL", "Direct Coombs",
-				"Indirect Coombs", "Blood Test NOS"],
+          "Viral Load", "Cryptococcal Antigen", "Lactate", "Fasting blood sugar", "Random blood sugar", "Sugar profile",
+          "Liver function test", "Hepatitis test", "Sickling test", "ESR", "Culture & sensitivity", "Widal test", "ELISA",
+          "ASO titre", "Rheumatoid factor", "Cholesterol", "Triglycerides", "Calcium", "Creatinine", "VDRL", "Direct Coombs",
+          "Indirect Coombs", "Blood Test NOS"],
 				"CSF" => ["Full CSF analysis", "Indian ink", "Protein & sugar", "White cell count", "Culture & sensitivity"],
 				"Urine" => ["Urine microscopy", "Urinanalysis", "Culture & sensitivity"],
 				"Aspirate" => ["Full aspirate analysis"],
@@ -591,7 +601,7 @@ class EncountersController < GenericEncountersController
 	def is_first_art_visit(patient_id)
 		session_date = session[:datetime].to_date rescue Date.today
 		art_encounter = Encounter.find(:first,:conditions =>["voided = 0 AND patient_id = ? AND encounter_type = ? AND DATE(encounter_datetime) < ?",
-		patient_id, EncounterType.find_by_name('ART_INITIAL').id, session_date ]) rescue nil
+        patient_id, EncounterType.find_by_name('ART_INITIAL').id, session_date ]) rescue nil
 		return true if art_encounter.nil?
 		return false
 	end
@@ -603,9 +613,9 @@ class EncountersController < GenericEncountersController
 
 		@diagnoses_detail = ConceptName.find(:all, :joins => :concept,
 			:conditions => ["concept_name.concept_id IN (?) AND voided = 0",
-		ConceptSet.find(:all, :conditions => ["concept_set IN (?)", ConceptName.find(:all, :joins => :concept,
-			:conditions => ["voided = 0 AND name = ?", diagnosis]).collect{|nom| nom.concept_id}]).collect{|set|
-		set.concept_id}]).collect{|term| term.name}.uniq
+        ConceptSet.find(:all, :conditions => ["concept_set IN (?)", ConceptName.find(:all, :joins => :concept,
+              :conditions => ["voided = 0 AND name = ?", diagnosis]).collect{|nom| nom.concept_id}]).collect{|set|
+          set.concept_id}]).collect{|term| term.name}.uniq
 		render :text => "<li></li><li>" + @diagnoses_detail.join("</li><li>") + "</li>"
 	end
   
