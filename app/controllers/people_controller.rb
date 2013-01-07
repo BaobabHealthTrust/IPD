49091902468ca43a301d9eeb8ea93c586751aec5
Ernest Matola <mangochiman@gmail.com>
2013-01-07 15:25:13 +0200

    removed the create method from people controller to be in generic people controller only

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -20,61 +20,8 @@ class PeopleController < GenericPeopleController
 	  @patient_bean = PatientService.get_patient(@person)
     render :layout => false
   end
- 
-  def create
-    success = false
-    Person.session_datetime = session[:datetime].to_date rescue Date.today
 
-    #for now BART2 will use BART1 for patient/person creation until we upgrade BART1 to 2
-    #if GlobalProperty.find_by_property('create.from.remote') and property_value == 'yes'
-    #then we create person from remote machine
-    if create_from_remote
-      person_from_remote = PatientService.create_remote_person(params)
-      person = PatientService.create_from_form(person_from_remote["person"]) unless person_from_remote.blank?
-
-      if !person.blank?
-        success = true
-        person.patient.remote_national_id
-      end
-    else
-      success = true
-      person = PatientService.create_from_form(params[:person])
-    end
-
-    if params[:person][:patient] && success
-      PatientService.patient_national_id_label(person.patient)
-      unless (params[:relation].blank?)
-        redirect_to search_complete_url(person.id, params[:relation]) and return
-      else
-
-       tb_session = false
-       if current_user.activities.include?('Manage Lab Orders') or current_user.activities.include?('Manage Lab Results') or
-        current_user.activities.include?('Manage Sputum Submissions') or current_user.activities.include?('Manage TB Clinic Visits') or
-         current_user.activities.include?('Manage TB Reception Visits') or current_user.activities.include?('Manage TB Registration Visits') or
-          current_user.activities.include?('Manage HIV Status Visits')
-         tb_session = true
-       end
-
-        if use_filing_number and not tb_session
-          PatientService.set_patient_filing_number(person.patient) 
-          archived_patient = PatientService.patient_to_be_archived(person.patient)
-          message = PatientService.patient_printing_message(person.patient,archived_patient,creating_new_patient = true)
-          unless message.blank?
-            print_and_redirect("/patients/filing_number_and_national_id?patient_id=#{person.id}" , next_task(person.patient),message,true,person.id)
-          else
-            print_and_redirect("/patients/filing_number_and_national_id?patient_id=#{person.id}", next_task(person.patient)) 
-          end
-        else
-          print_and_redirect("/patients/national_id_label?patient_id=#{person.id}", next_task(person.patient))
-        end
-      end
-    else
-      # Does this ever get hit?
-      redirect_to :action => "index"
-    end
-  end
-
-    def search
+  def search
 		found_person = nil
 		if params[:identifier]
 			local_results = PatientService.search_by_identifier(params[:identifier])
