c8005d19261eef1bc96d6203e63a43e653bbdeb0
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-01-17 14:50:53 +0200

    adding a dde search by name functionality

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -52,15 +52,86 @@ class PeopleController < GenericPeopleController
 				end
 			end
 		end
-		@relation = params[:relation]
-		@people = PatientService.person_search(params)
-		@patients = []
-		@people.each do | person |
-			patient = PatientService.get_patient(person) rescue nil
-			@patients << patient
-		end
+    @relation = params[:relation]                                               
+    @people = PatientService.person_search(params)                              
+    @search_results = {}                                                        
+    @patients = []                                                              
+                                                                                
+    (PatientService.search_from_remote(params) || []).each do |data|            
+      results = PersonSearch.new(data["npid"]["value"])                         
+      results.national_id = data["npid"]["value"]                               
+      results.current_residence =data["person"]["data"]["addresses"]["city_village"]
+      results.person_id = 0                                                     
+      results.home_district = data["person"]["data"]["addresses"]["state_province"]
+      results.traditional_authority =  data["person"]["data"]["addresses"]["county_district"]
+      results.name = data["person"]["data"]["names"]["given_name"] + " " + data["person"]["data"]["names"]["family_name"]
+      gender = data["person"]["data"]["gender"]                                 
+      results.occupation = data["person"]["data"]["occupation"]                 
+      results.sex = (gender == 'M' ? 'Male' : 'Female')                         
+      results.birthdate_estimated = (data["person"]["data"]["birthdate_estimated"]).to_i
+      results.birth_date = birthdate_formatted((data["person"]["data"]["birthdate"]).to_date , results.birthdate_estimated)
+      results.birthdate = (data["person"]["data"]["birthdate"]).to_date         
+      results.age = cul_age(results.birthdate.to_date , results.birthdate_estimated)
+      @search_results[results.national_id] = results                            
+    end if create_from_dde_server                                               
+                                                                                
+    (@people || []).each do | person |                                          
+      patient = PatientService.get_patient(person) rescue nil                   
+      next if patient.blank?                                                    
+      results = PersonSearch.new(patient.national_id || patient.patient_id)     
+      results.national_id = patient.national_id                                 
+      results.birth_date = patient.birth_date                                   
+      results.current_residence = patient.current_residence                     
+      results.guardian = patient.guardian                                       
+      results.person_id = patient.person_id                                     
+      results.home_district = patient.home_district                             
+      results.traditional_authority = patient.traditional_authority             
+      results.mothers_surname = patient.mothers_surname                         
+      results.dead = patient.dead                                               
+      results.arv_number = patient.arv_number                                   
+      results.eid_number = patient.eid_number                                   
+      results.pre_art_number = patient.pre_art_number                           
+      results.name = patient.name                                               
+      results.sex = patient.sex                                                 
+      results.age = patient.age                                                 
+      @search_results.delete_if{|x,y| x == results.national_id}                 
+      @patients << results                                                      
+    end 
+ 
+    (@search_results || {}).each do |npid , data |                              
+      @patients << data                                                         
+    end 
 
 	end
 
+
+  protected
+
+  def cul_age(birthdate , birthdate_estimated , date_created = Date.today, today = Date.today)
+                                                                                
+    # This code which better accounts for leap years                            
+    patient_age = (today.year - birthdate.year) + ((today.month - birthdate.month) + ((today.day - birthdate.day) < 0 ? -1 : 0) < 0 ? -1 : 0)
+                                                                                
+    # If the birthdate was estimated this year, we round up the age, that way if
+    # it is March and the patient says they are 25, they stay 25 (not become 24)
+    birth_date = birthdate                                                      
+    estimate = birthdate_estimated == 1                                         
+    patient_age += (estimate && birth_date.month == 7 && birth_date.day == 1  &&
+        today.month < birth_date.month && date_created.year == today.year) ? 1 : 0
+  end                                                                           
+                                                                                
+  def birthdate_formatted(birthdate,birthdate_estimated)                        
+    if birthdate_estimated == 1                                                 
+      if birthdate.day == 1 and birthdate.month == 7                            
+        birthdate.strftime("??/???/%Y")                                         
+      elsif birthdate.day == 15                                                 
+        birthdate.strftime("??/%b/%Y")                                          
+      elsif birthdate.day == 1 and birthdate.month == 1                         
+        birthdate.strftime("??/???/%Y")                                         
+      end                                                                       
+    else                                                                        
+      birthdate.strftime("%d/%b/%Y")                                            
+    end                                                                         
+  end
 end
  
---
+++ app/models/person_search.rb
@@ -0,0 +1,11 @@
+class PersonSearch
+                                                                                
+  attr_accessor :person_id, :name, :age, :sex , :national_id,:arv_number ,:pre_art_number                 
+  attr_accessor :birth_date, :current_residence, :guardian, :dead, :eid_number,:occupation
+  attr_accessor :home_district, :traditional_authority,:mothers_surname ,:birthdate_estimated
+  attr_accessor :cell_phone_number, :home_phone_number, :office_phone_number, :birthdate
+                                                                                
+  def initialize(passed_national_id)  
+  end
+
+end
