282e5532bfacc2c0da3613d7f519f117324b8e0c
Ernest Matola <mangochiman@gmail.com>
2013-07-02 13:52:58 +0200

    skipping before filter for printable reports

--- app/controllers/generic_application_controller.rb
+++ app/controllers/generic_application_controller.rb
@@ -25,15 +25,20 @@ class GenericApplicationController < ActionController::Base
 	helper_method :next_task
 	filter_parameter_logging :password
 	before_filter :authenticate_user!, :except => ['login', 'logout','remote_demographics',
-		                                      'create_remote', 'mastercard_printable', 'get_token', 'admission_form','admission_form_printable']
+		                                      'create_remote', 'mastercard_printable', 'get_token', 
+                                          'admission_form','admission_form_printable',
+                                          'adt_generic_report_printable','adt_report_by_ward_printable']
 
     before_filter :set_current_user, :except => ['login', 'logout','remote_demographics',
-		                                      'create_remote', 'mastercard_printable', 'get_token', 'admission_form','admission_form_printable']
+		                                      'create_remote', 'mastercard_printable', 'get_token', 
+                                          'admission_form','admission_form_printable',
+                                          'adt_generic_report_printable','adt_report_by_ward_printable']
 
 	before_filter :location_required, :except => ['login', 'logout', 'location',
 		                                        'demographics','create_remote',
 		                                         'mastercard_printable',
-		                                        'remote_demographics', 'get_token', 'single_sign_in', 'admission_form','admission_form_printable']
+		                                        'remote_demographics', 'get_token', 'single_sign_in', 'admission_form',
+                                            'admission_form_printable','adt_generic_report_printable','adt_report_by_ward_printable']
   before_filter :set_current_team
 	def rescue_action_in_public(exception)
 		@message = exception.message
